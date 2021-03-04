c4db4e24cd53664843288afcfa79c426da8408ea
Sara <sbr.90@hotmail.com>
2021-03-04 10:26:33 +0100

    added addAttendee check in eventTest

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,2 +1,16 @@
+import com.example.Attendee;
+import com.example.Event;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class EventTest {
+    @Test
+    @DisplayName("Check add attendes")
+    public void addAttendee() {
+        Event event = new Event();
+        Attendee attendee = new Attendee(1L,"Roberto", "roberto@example.com");
+        event.addAttendee(attendee);
+        assertTrue(event.getAttendees().contains(attendee));
+    }
 }
